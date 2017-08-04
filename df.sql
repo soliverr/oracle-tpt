58f867fb30e214418c9c316c85595d017103a6bf
sOliverr <soliverr@gmail.com>
2017-08-04 14:11:23 +0500

    Total MB depends on MaxBytes for autoextensibled files

--- df.sql
+++ df.sql
@@ -13,9 +13,22 @@
 col "% Used" for a6
 col "Used" for a22
 
-select t.tablespace_name, t.mb "TotalMB", t.mb - nvl(f.mb,0) "UsedMB", nvl(f.mb,0) "FreeMB"
-       ,lpad(ceil((1-nvl(f.mb,0)/decode(t.mb,0,1,t.mb))*100)||'%', 6) "% Used", t.ext "Ext", 
-       '|'||rpad(lpad('#',ceil((1-nvl(f.mb,0)/decode(t.mb,0,1,t.mb))*20),'#'),20,' ')||'|' "Used"
+select tablespace_name
+      ,t_mb "TotalMB"
+      ,t_mb - f_mb "UsedMB"
+      ,f_mb "FreeMB"
+      ,lpad(ceil((1 - f_mb / decode(t_mb, 0, 1, t_mb)) * 100) || '%', 6) "% Used"
+      ,t_ext "Ext"
+      ,'|' || rpad(lpad('#', ceil((1 - f_mb / decode(t_mb, 0, 1, t_mb)) * 20), '#'), 20, ' ') || '|' "Used"
+from (
+select t.tablespace_name as tablespace_name,
+       t.maxmb,
+       t.mb,
+       case when t.ext = 'YES' then nvl(t.maxmb, nvl(t.mb, 0))
+            else nvl(t.mb, 0)
+       end as t_mb,
+       nvl(f.mb,0) as f_mb,
+       t.ext as t_ext
 from (
   select tablespace_name, trunc(sum(bytes)/1048576) MB
   from dba_free_space
@@ -25,14 +38,15 @@ from (
   from v$temp_space_header
   group by tablespace_name
 ) f, (
-  select tablespace_name, trunc(sum(bytes)/1048576) MB, max(autoextensible) ext
+  select tablespace_name, trunc(sum(bytes)/1048576) MB, trunc(sum(maxbytes)/1048576) MaxMB, max(autoextensible) ext
   from dba_data_files
   group by tablespace_name
  union all
-  select tablespace_name, trunc(sum(bytes)/1048576) MB, max(autoextensible) ext
+  select tablespace_name, trunc(sum(bytes)/1048576) MB, trunc(sum(maxbytes)/1048576) MaxMB, max(autoextensible) ext
   from dba_temp_files
   group by tablespace_name
 ) t
 where t.tablespace_name = f.tablespace_name (+)
-order by t.tablespace_name;
+)
+order by tablespace_name;
 
