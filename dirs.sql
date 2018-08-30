8fc42329115c6ccf27cd3ee3a854ac8e12a9daaf
sOliverr <soliverr@gmail.com>
2018-08-30 11:52:43 +0500

    Usage info

--- dirs.sql
+++ dirs.sql
@@ -1,3 +1,23 @@
-col dirs_directory_path head DIRECTORY_PATH for a90
+--------------------------------------------------------------------------------
+--
+-- File name:   dirs.sql
+-- Purpose:     List database directories
+--
+-- Author:      Sergey Kryazhevskikh, <soliverr@gmail.com>
+-- Copyright:   (c) Public Domain
+--
+-- Usage:       List available directories in database.
+--              
+--              This script lists all available directories object 
+--              from dba_directories table.
+--              
+--              @dirs
+--
+--------------------------------------------------------------------------------
 
-select owner, directory_name, directory_path dirs_directory_path from dba_directories;
+column dirs_directory_path head DIRECTORY_PATH format a64
+column directory_name format a32
+
+select owner, directory_name, directory_path dirs_directory_path 
+  from dba_directories
+;
