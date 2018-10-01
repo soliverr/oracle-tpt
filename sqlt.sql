d04b4ce1177f811f736e6e62eb095bbff6f55455
sOliverr <soliverr@gmail.com>
2018-10-01 13:15:27 +0500

    Exclude query from output

--- sqlt.sql
+++ sqlt.sql
@@ -1,17 +1,37 @@
+--------------------------------------------------------------------------------
+--
+-- File name:   sqlt.sql
+-- Purpose:     Search SQL by text substring
+--
+-- Author:      Tanel Poder
+--              Sergey Kryazhevskikh, <soliverr@gmail.com>
+-- Copyright:   (c) Public Domain
+--
+-- Usage:       List SQLs with given substring in it.
+--              
+--              This script lists SQL from V$SQL system view
+--              
+--              @sqlt some_sql_text
+--
+--------------------------------------------------------------------------------
+
+column sql_id           format a17
+column optimizer_mode   heading OPT_MODE format a17
+column child_number     heading CHLD#
 column sqlt_sql_text	heading SQL_TEXT format a100 word_wrap
 
-select 
-	hash_value, 
---    sql_id,
---	old_hash_value,
-	child_number chld#, 
---	plan_hash_value plan_hash, 
-	optimizer_mode opt_mode,
-	sql_text sqlt_sql_text
-from 
-	v$sql 
-where 
-	lower(sql_text) like lower('%&1%')
---and	hash_value != (select sql_hash_value from v$session where sid = (select sid from v$mystat where rownum = 1))
+select /* oradba-tpt */
+        sql_id,
+    	hash_value, 
+--	      old_hash_value,
+    	child_number, 
+--	      plan_hash_value plan_hash, 
+    	optimizer_mode,
+	    sql_text sqlt_sql_text
+  from 
+       v$sql 
+ where 
+ 	   lower(sql_text) like lower('%&1%')
+   and sql_text not like '%/* oradba-tpt */%'    
 /
 
